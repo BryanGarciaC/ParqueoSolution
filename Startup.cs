a02d58c62926e27b5bacf6c17971a619152d340e
Bryan Garcia Castillo <bgarciac7@miumg.edu.gt>
2021-10-28 01:38:02 -0600

    Agregar archivos de proyecto.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ParqueoSolution.Startup))]
+namespace ParqueoSolution
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
